bb7a910a5c53d059c2f5d1236cff25f4360f3528
kratika <kratikan235@gmail.com>
2026-04-14 18:19:38 +0530

    Create Project APIs

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.api.auth import router as auth_router
 from app.api.user import router as user_router
+from app.api import project
 from app.middleware.tenant_middleware import TenantMiddleware
 from fastapi.security import HTTPBearer
 
@@ -12,3 +13,4 @@ security = HTTPBearer()
 
 app.include_router(user_router)
 app.include_router(auth_router)
+app.include_router(project.router)
